c395bb9a22c41cdc3e2476fc65fd82744364e8e0
Marto <martinarto2015@gmail.com>
2025-04-01 09:23:33 -0300

    Envs

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBm79j87JC6WmzBs31BA2BXQ16jPxhINOI",
-  authDomain: "blog-ia-50b1c.firebaseapp.com",
-  projectId: "blog-ia-50b1c",
-  storageBucket: "blog-ia-50b1c.firebasestorage.app",
-  messagingSenderId: "575253328352",
-  appId: "1:575253328352:web:20595872fd10d353f8bb85",
-  measurementId: "G-EBXXPCR3MM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
